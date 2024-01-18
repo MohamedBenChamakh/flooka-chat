87270f4882c8e1be794b2193b039f4f763691d6e
Mohamed Ben Chamakh <mohamed.ben.chamakh98@gmail.com>
2024-01-18 15:42:51 +0100

    commit

--- flooka-chat/src/app/pages/menu/menu.component.ts
+++ flooka-chat/src/app/pages/menu/menu.component.ts
@@ -8,29 +8,54 @@ import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/User';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent implements OnInit, OnDestroy {
   destroy$!: Subject<any>;
   rooms: Room[] = [];
+  formGroup!: FormGroup;
 
   ngOnInit(): void {
     feather.replace({ 'width': 20, 'height': 20 });
     this.destroy$ = new Subject();
     this.loadChatrooms();
+    this.initForm();
   }
 
   constructor(
     private chatService: ChatService,
-    private userService: UserService
+    private userService: UserService,
+    private formBuilder: FormBuilder
   ) {
 
+  }
+
+  initForm() {
+    this.formGroup = this.formBuilder.group({
+      age: [18],
+      country: ["Tunisia"],
+      gender: ["Men"]
+    });
+  }
+
+  onSubmit() {
+    const formValue = this.formGroup.value;
+    const userId = localStorage.getItem("user_id");
+    this.chatService.createRoom(formValue).subscribe({
+      next: (room: any) => {
+        room.members = room.members.filter((id: string) => id !== userId);
+        this.getUserById(room.members[0]).then(user => room.members[0] = user);
+        this.rooms.push(room);
+      },
+      error: (err) => console.log(err)
+    })
   }
 
 
